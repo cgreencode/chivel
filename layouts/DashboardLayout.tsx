774ce1a12c2329efdb51d17b2371648316c53172
anproagile <anproagile@gmail.com>
2021-12-12 18:54:40 +0545

    Little Improvement

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -3,65 +3,61 @@ import { BiCode, BiNavigation } from 'react-icons/bi';
 import { FiSettings, FiInbox } from 'react-icons/fi';
 import { VscGraphLine } from 'react-icons/vsc';
 import clsx from 'clsx';
-import { useRouter } from 'next/router';
 
 const DashboardLayout: React.FC<{
   heading: string;
   description: string;
   page: 'setup' | 'seo' | 'navbar' | 'settings' | 'snippet';
 }> = ({ page, heading, description, ...props }) => {
-  const router = useRouter();
-  const { id } = router.query;
-
   return (
     <div className='flex text-white bg-black'>
       <div className='w-1/5 h-screen px-8 pt-24 border-r border-gray-800'>
         <div className='space-y-4'>
-          <Link href={`/site/${id}/setup`}>
+          <Link href='/dashboard/setup'>
             <a
               className={clsx(
                 'block w-full px-4 py-2 text-gray-300 rounded hover:bg-gray-900 hover:text-gray-50',
-                page === 'setup' && 'bg-gray-900 text-gray-50 font-bold'
+                page === 'setup' && 'bg-gray-900 text-gray-50'
               )}>
               <FiInbox className='relative inline-block mr-3 bottom-px' />
               Setup
             </a>
           </Link>
-          <Link href={`/site/${id}/code`}>
+          <Link href='/dashboard/snippet'>
             <a
               className={clsx(
                 'block w-full px-4 py-2 text-gray-300 rounded hover:bg-gray-900 hover:text-gray-50',
-                page === 'snippet' && 'bg-gray-900 text-gray-50 font-bold'
+                page === 'snippet' && 'bg-gray-900 text-gray-50'
               )}>
               <BiCode className='relative inline-block mr-3 bottom-px' />
               Snippet Injection
             </a>
           </Link>
-          <Link href={`/site/${id}/navbar`}>
+          <Link href='/dashboard/reviews'>
             <a
               className={clsx(
                 'block w-full px-4 py-2 text-gray-300 rounded hover:bg-gray-900 hover:text-gray-50',
-                page === 'navbar' && 'bg-gray-900 text-gray-50 font-bold'
+                page === 'navbar' && 'bg-gray-900 text-gray-50'
               )}>
               <BiNavigation className='relative inline-block mr-3 bottom-px' />
               Navbar
             </a>
           </Link>
-          <Link href={`/site/${id}/seo`}>
+          <Link href='/dashboard/seo'>
             <a
               className={clsx(
                 'block w-full px-4 py-2 text-gray-300 rounded hover:bg-gray-900 hover:text-gray-50',
-                page === 'seo' && 'bg-gray-900 text-gray-50 font-bold'
+                page === 'seo' && 'bg-gray-900 text-gray-50'
               )}>
               <VscGraphLine className='relative inline-block mr-3 bottom-px' />
               SEO
             </a>
           </Link>
-          <Link href={`/site/${id}/settings`}>
+          <Link href='/dashboard/settings'>
             <a
               className={clsx(
                 'block w-full px-4 py-2 text-gray-300 rounded hover:bg-gray-900 hover:text-gray-50',
-                page === 'settings' && 'bg-gray-900 text-gray-50 font-bold'
+                page === 'settings' && 'bg-gray-900 text-gray-50'
               )}>
               <FiSettings className='relative inline-block mr-3 bottom-px' />
               Settings
